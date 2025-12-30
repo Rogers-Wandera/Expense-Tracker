0a6dc49960e71ad889ff85e0389c7b00c47d7bf3
Rogers-Wandera <rogerrisha@gmail.com>
2025-12-30 18:37:03 +0300

    fixed import issues in the hoc

--- lib/hocs/with-auth.tsx
+++ lib/hocs/with-auth.tsx
@@ -4,7 +4,7 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { Spinner } from "@heroui/react";
-import { UserRole } from "@prisma/client";
+import { UserRole } from "@/generated/prisma";
 
 interface WithAuthProps {
   children: React.ReactNode;
